28745c9f05947a17684ed0089096e33d3861ede2
Namdrib <ph0fgazfl4j1@opayq.com>
2023-02-28 22:47:14 +1030

    update install.sh
    - create ~/.dotfiles symlink
    - use absolute paths to shell commands to prevent command hijacking
    - safer use of globs

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 # Get current dir (so run this script from anywhere)
 export DOTFILES_DIR DOTFILES_CACHE DOTFILES_EXTRA_DIR DOTFILES_HOME
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOTFILES_DIR="$( /usr/bin/cd "$( dirname "${BASH_SOURCE[0]}" )" && /usr/bin/pwd )"
 DOTFILES_CACHE="$DOTFILES_DIR/.cache.sh"
 DOTFILES_EXTRA_DIR="$HOME/.extra"
 DOTFILES_HOME="$HOME/.dotfiles"
@@ -13,17 +13,17 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Update dotfiles itself first
 if is-executable git -a -d "$DOTFILES_DIR/.git"; then
-	git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+	/usr/bin/git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 fi
 
 # Create home dotfiles symlink
-# ln -sfv "$DOTFILES_DIR/" "$DOTFILES_HOME"
+/usr/bin/ln -sfv "$DOTFILES_DIR/" "$DOTFILES_HOME"
 
 # Specifically link dotfiles to the home one
-linkables=$( ls -d **/*.symlink )
+linkables=$( /usr/bin/ls -d -- **/*.symlink )
 for file in $linkables ;do
-	target="$HOME/.$( basename "$file" ".symlink" )"
-	ln -sfv "$DOTFILES_HOME/$file" "$target"
+	target="$HOME/.$( /usr/bin/basename "$file" ".symlink" )"
+	/usr/bin/ln -sfv "$DOTFILES_HOME/$file" "$target"
 done
 
 # Install extra stuff
