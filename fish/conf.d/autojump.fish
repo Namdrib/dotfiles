b9ca88467ad570d97e2e463aae7c11aa966e95c2
Namdrib <ph0fgazfl4j1@opayq.com>
2025-08-10 22:32:48 +0930

    fish: Source the correct autojump file on Arch

--- fish/conf.d/autojump.fish
+++ fish/conf.d/autojump.fish
@@ -1,5 +1,13 @@
 # Source autojump setup if it exists
-# Arch, Fedora, Ubuntu
+# Check the autojump* package listing to see which files exist on each distro
+
+# Arch
+if [ -f /etc/fish/conf.d/autojump.fish ]
+	set fish_function_path $fish_function_path "/etc/fish/conf.d"
+	source /etc/fish/conf.d/autojump.fish
+end
+
+# Fedora, Ubuntu
 if [ -f /usr/share/autojump/autojump.fish ]
 	set fish_function_path $fish_function_path "/usr/share/autojump"
 	source /usr/share/autojump/autojump.fish
