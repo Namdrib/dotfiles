d9ff1eeacddd0ac6d4a3c1945de7a74db6fc7dcc
Namdrib <ph0fgazfl4j1@opayq.com>
2025-10-11 12:11:18 +1030

    darkman: Only run background on niri

--- wayland/darkman/dark-mode.d/swaybg.sh
+++ wayland/darkman/dark-mode.d/swaybg.sh
@@ -4,5 +4,7 @@ IMG_PATH=/usr/share/wallpapers/Bamboo\ at\ Night
 LANDSCAPE_IMG="${IMG_PATH}/contents/images/5120x2880.png"
 PORTRAIT_IMG="${IMG_PATH}/contents/images/5120x2880.png"
 
-swaybg --output DP-1 --image "${LANDSCAPE_IMG}" &
-swaybg --output DP-3 --image "${PORTRAIT_IMG}" --mode center &
+if pgrep niri; then
+	swaybg --output DP-1 --image "${LANDSCAPE_IMG}" &
+	swaybg --output DP-3 --image "${PORTRAIT_IMG}" --mode center &
+fi
