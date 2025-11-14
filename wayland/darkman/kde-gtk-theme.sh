590690335b9e7c32b0b781b77452de42300f1812
Namdrib <ph0fgazfl4j1@opayq.com>
2025-11-14 21:00:59 +1030

    darkman: Unify GTK theming script

--- wayland/darkman/kde-gtk-theme.sh
+++ wayland/darkman/kde-gtk-theme.sh
@@ -4,4 +4,10 @@
 # GTK themes can be installed here: Global Theme > Application Style > Configure GNOME/GTK Application Style.
 # Reference: https://wiki.archlinux.org/title/Uniform_look_for_Qt_and_GTK_applications
 
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
+case "$1" in
+	dark) THEME=Adwaita-dark ;;
+	light) THEME=Adwaita-dark ;;
+	default) exit 1 ;;
+esac
+
+gsettings set org.gnome.desktop.interface gtk-theme "${THEME}"
--- wayland/darkman/light-mode.d/kde-gtk-theme.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# GTK apps are not affected by the Plasma Global Theme, which only applies to Qt based programs.
-# GTK themes can be installed here: Global Theme > Application Style > Configure GNOME/GTK Application Style.
-# Reference: https://wiki.archlinux.org/title/Uniform_look_for_Qt_and_GTK_applications
-
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
