c3a75c005ff41e974f0127af63adf2a04ac9feef
Namdrib <ph0fgazfl4j1@opayq.com>
2025-11-14 20:53:50 +1030

    darkman: Refactor to account for complex-stonework
    The Bamboo wallpapers are not available on Nobara by default - using the
    default Fedora KDE wallpapers for complex-stonework
    
    Set for a single landscape monitor on a different output

--- wayland/darkman/light-mode.d/swaybg.sh
+++ wayland/darkman/light-mode.d/swaybg.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-IMG_PATH=/usr/share/wallpapers/Bamboo
-LANDSCAPE_IMG="${IMG_PATH}/contents/images/5120x2880.png"
-PORTRAIT_IMG="${IMG_PATH}/contents/images/5120x2880.png"
+# The wallpaper directories
+WALLPAPERS_DIR=/usr/share/wallpapers
+IMG_1_DIR="${WALLPAPERS_DIR}/Bamboo/contents/images"
+IMG_2_DIR="${WALLPAPERS_DIR}/Mountain/contents/images"
+
+# The specific images to use
+LANDSCAPE_IMG="${IMG_1_DIR}/5120x2880.png"
+PORTRAIT_IMG="${IMG_1_DIR}/5120x2880.png"
+LANDSCAPE_2_IMG="${IMG_2_DIR}/5120x2880.png"
 
 if pgrep niri; then
-	swaybg --output "DP-1" --image "${LANDSCAPE_IMG}" &
-	swaybg --output "DP-3" --image "${PORTRAIT_IMG}" --mode center &
+	swaybg --output DP-1 --image "${LANDSCAPE_IMG}" &
+	swaybg --output DP-3 --image "${PORTRAIT_IMG}" --mode center &
+	swaybg --output HDMI-A-2 --image "${LANDSCAPE_2_IMG}" &
 fi
