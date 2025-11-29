c7f2d58cb62133663321b612a9bb81401b2e214d
Namdrib <ph0fgazfl4j1@opayq.com>
2025-11-29 15:56:02 +1030

    darkman: Add GTK theming for KDE

--- wayland/darkman/kde-gtk-theme.sh
+++ wayland/darkman/kde-gtk-theme.sh
@@ -5,9 +5,16 @@
 # Reference: https://wiki.archlinux.org/title/Uniform_look_for_Qt_and_GTK_applications
 
 case "$1" in
-	dark) THEME=Adwaita-dark ;;
-	light) THEME=Adwaita-dark ;;
+	dark)
+		THEME=Adwaita-dark
+		THEME2=Breeze-dark-gtk
+		;;
+	light)
+		THEME=Adwaita-dark
+		THEME2=Breeze-dark-gtk
+		;;
 	default) exit 1 ;;
 esac
 
+dbus-send --session --dest=org.kde.GtkConfig --type=method_call /GtkConfig org.kde.GtkConfig.setGtkTheme "string:${THEME2}"
 gsettings set org.gnome.desktop.interface gtk-theme "${THEME}"
