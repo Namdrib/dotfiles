1746d515097bfb03b694ec44338dcb100204b4f7
Namdrib <ph0fgazfl4j1@opayq.com>
2026-01-10 16:28:51 +1030

    fish: Fix powerline error on fish 4.3+
    See: https://github.com/powerline/powerline/issues/2281
    
    Hopefully this is just a temporary workaround that can be removed,
    eventually

--- fish/conf.d/powerline.fish
+++ fish/conf.d/powerline.fish
@@ -1,4 +1,9 @@
 # Source powerline setup if it exists
+
+# Required on fish 4.3+, otherwise the powerline setup fails
+# See: set --global fish_key_bindings fish_default_key_bindings
+set --global fish_key_bindings fish_default_key_bindings
+
 # Arch, Ubuntu
 if [ -f /usr/share/powerline/bindings/fish/powerline-setup.fish ]
 	set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
