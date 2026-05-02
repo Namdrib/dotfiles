29495cb154c643bbcae262c3af957a6ef519d57a
Namdrib <ph0fgazfl4j1@opayq.com>
2026-05-02 22:57:45 +0930

    wayland: Address ShellCheck warnings

--- wayland/install.sh
+++ wayland/install.sh
@@ -11,23 +11,23 @@ mkdir -pv \
 	~/.config/xdg-desktop-portal \
 	~/.local/share/darkman
 
-ln -svfT $(realpath fuzzel) ~/.config/fuzzel
-ln -svf $(realpath mako.cfg) ~/.config/mako/config
-ln -svf $(realpath niri/binds.kdl) ~/.config/niri/binds.kdl
-ln -svf $(realpath niri/config.kdl) ~/.config/niri/config.kdl
-ln -svf $(realpath niri/layout.kdl) ~/.config/niri/layout.kdl
-ln -svf $(realpath niri/output.kdl) ~/.config/niri/output.kdl
-ln -svf $(realpath niri/rules.kdl) ~/.config/niri/rules.kdl
-ln -svf $(realpath swayidle.cfg) ~/.config/swayidle/config
-ln -svf $(realpath swaylock.cfg) ~/.config/swaylock/config
-ln -svf $(realpath waybar-config.jsonc) ~/.config/waybar/config.jsonc
-ln -svf $(realpath waybar-style.css) ~/.config/waybar/style.css
-ln -svf $(realpath waybar-power_menu.xml) ~/.config/waybar/power_menu.xml
+ln -svfT "$(realpath fuzzel)" ~/.config/fuzzel
+ln -svf "$(realpath mako.cfg)" ~/.config/mako/config
+ln -svf "$(realpath niri/binds.kdl)" ~/.config/niri/binds.kdl
+ln -svf "$(realpath niri/config.kdl)" ~/.config/niri/config.kdl
+ln -svf "$(realpath niri/layout.kdl)" ~/.config/niri/layout.kdl
+ln -svf "$(realpath niri/output.kdl)" ~/.config/niri/output.kdl
+ln -svf "$(realpath niri/rules.kdl)" ~/.config/niri/rules.kdl
+ln -svf "$(realpath swayidle.cfg)" ~/.config/swayidle/config
+ln -svf "$(realpath swaylock.cfg)" ~/.config/swaylock/config
+ln -svf "$(realpath waybar-config.jsonc)" ~/.config/waybar/config.jsonc
+ln -svf "$(realpath waybar-style.css)" ~/.config/waybar/style.css
+ln -svf "$(realpath waybar-power_menu.xml)" ~/.config/waybar/power_menu.xml
 # Use KDE backends for things like file picker, secrets
-ln -svf $(realpath niri-portals.conf) ~/.config/xdg-desktop-portal/niri-portals.conf
+ln -svf "$(realpath niri-portals.conf)" ~/.config/xdg-desktop-portal/niri-portals.conf
 
-ln -svfT $(realpath darkman/config.yaml) ~/.config/darkman/config.yaml
-ln -svfT $(realpath darkman/mode-scripts) ~/.local/share/darkman
+ln -svfT "$(realpath darkman/config.yaml)" ~/.config/darkman/config.yaml
+ln -svfT "$(realpath darkman/mode-scripts)" ~/.local/share/darkman
 # Point the old paths to the new one for backwards compatibility
 ln -svfT ~/.local/share/darkman ~/.local/share/dark-mode.d
 ln -svfT ~/.local/share/darkman ~/.local/share/light-mode.d
